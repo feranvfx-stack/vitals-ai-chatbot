6d22a5b813d12ab22d2108fe6a45e728ede2448a
FeranVFX <feranvfx@gmail.com>
2026-08-19 16:36:28 +0100

    fix: Caddy

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const GROQ_API_KEY = "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";
 export const GROQ_MODEL = "llama-3.3-70b-versatile";
 export const SYSTEM_PROMPT = "";
+// export const API_BASE = import.meta.env.VITE_API_URL;
